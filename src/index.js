d9ca9f12c4906062a60db9128eecf959e809c3d9
Jafr0 <87328407+Jafr0@users.noreply.github.com>
2022-02-22 12:27:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ module.exports = function towelSort(matrix) {
 	if (!matrix.length) {
 		return []
 	}
-
+    
+if (matrix === undefined) {
+		return []
+	}
 
 	var result = matrix.map((e, i) => (i % 2) ? e.toString().split(',').reverse() : e.toString().split(',')).join().split(",");
 
